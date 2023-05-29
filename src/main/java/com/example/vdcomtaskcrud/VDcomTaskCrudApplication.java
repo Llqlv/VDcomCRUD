da988a5159ec241af75af5d70c999c53bf6f6829
Llqlv <ivanlalkq@gmail.com>
2023-05-29 22:53:07 +0300

    Start project

---
+++ src/main/java/com/example/vdcomtaskcrud/VDcomTaskCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.vdcomtaskcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VDcomTaskCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VDcomTaskCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vdcomtaskcrud/VDcomTaskCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vdcomtaskcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VDcomTaskCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
